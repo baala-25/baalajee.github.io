170b202ce1f5a8b6950128f0fb6d01c5fcde1a81
baala-25 <142534134+baala-25@users.noreply.github.com>
2023-08-18 01:17:19 +0530

    Add files via upload
    initial hub

---
+++ script.js
@@ -0,0 +1,24 @@
+// header scrolling effect
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+      $('header').addClass('nav-show');
+		  
+	} 
+	else{
+		$('header').removeClass('nav-show');
+	}
+	   
+})
+
+
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	  
+
+window.onload = () => navSlide();
